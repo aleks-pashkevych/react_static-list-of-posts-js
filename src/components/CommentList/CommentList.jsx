75e5294e7533f8f5a1f9e89f94bee61154e77109
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-03-12 19:42:49 +0200

    task done uncorrectly, need to check

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,22 @@
-export const CommentList = () => <>Put the list here</>;
+export const CommentList = ({ data }) => {
+  return (
+    <div className="CommentInfo">
+      {data.comment.map(com => {
+        return (
+          <div key={com.id}>
+            <div className="CommentInfo__title">
+              <strong className="CommentInfo__name">{com.name}</strong>
+              {' by '}
+
+              <a className="CommentInfo__email" href={com.email}>
+                {com.email}
+              </a>
+            </div>
+
+            <div className="CommentInfo__body">{com.body}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,29 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ data }) => {
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{data.post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <a className="UserInfo" href={data.user.email}>
+            {data.user.name}
+          </a>
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{data.post.body}</p>
+
+      <hr />
+
+      {data.comment.length === 0 ? (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      ) : (
+        <CommentList data={data} />
+      )}
+    </div>
+  );
+};
